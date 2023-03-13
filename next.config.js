b9636efb6ea67bfee3bd5a83d782b0d42e0f7f19
jorgechato <hack@jorgechato.com>
2023-03-13 23:37:10 +0900

    ADD env var & tailwind

--- next.config.js
+++ next.config.js
@@ -5,6 +5,36 @@ const nextConfig = {
   },
   env: {
     ARTICLES_LOCATION: 'articles',
+    TITLE: 'WHAT IS JORGE</br>DOING TODAY?',
+    SITE_MAP: [
+      {
+        name: 'Home',
+        url: '/',
+      },
+      {
+        name: 'About',
+        url: '/about',
+      },
+      {
+        name: 'Archive',
+        url: '/archive',
+      },
+    ],
+
+    SOCIALS: [
+      {
+        name: 'GitHub',
+        url: '',
+      },
+      {
+        name: 'Twitter',
+        url: '',
+      },
+      {
+        name: 'LinkedIn',
+        url: '',
+      },
+    ],
   },
 }
 
