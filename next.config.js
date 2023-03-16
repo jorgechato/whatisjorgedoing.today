0b0c4ed50f999885652bacd88fc19e483bf39195
jorgechato <hack@jorgechato.com>
2023-03-16 22:10:06 +0900

    UPDATE dep, envs & ADD basic css

--- next.config.js
+++ next.config.js
@@ -4,35 +4,57 @@ const nextConfig = {
     appDir: true,
   },
   env: {
-    ARTICLES_LOCATION: 'articles',
+    ARTICLES_LOCATION: 'content/articles',
     TITLE: 'WHAT IS JORGE</br>DOING TODAY?',
     SITE_MAP: [
       {
         name: 'Home',
         url: '/',
+        location: 'content/home.md',
       },
       {
         name: 'About',
         url: '/about',
+        location: 'content/about.md',
       },
       {
         name: 'Archive',
         url: '/archive',
       },
     ],
+    ECHOSYSTEM: [
+      {
+        name: 'JORGE</br>CHATO',
+        description: 'Personal website',
+        url: 'https://jorgechato.com',
+        color: '',
+      },
+      {
+        name: 'WHERE IS</br>JORGE TODAY?',
+        description: 'Funny project to track my location and events',
+        url: 'https://whereisjorge.today',
+        color: '',
+      },
+      {
+        name: 'NO APTO</br>EN ASIA',
+        description: 'Podcast about my experience in Asia',
+        url: 'https://noaptoen.asia',
+        color: '',
+      },
+    ],
 
     SOCIALS: [
       {
         name: 'GitHub',
-        url: '',
+        url: 'https://github.com/jorgechato',
       },
       {
         name: 'Twitter',
-        url: '',
+        url: 'https://twitter.com/jorgechato',
       },
       {
         name: 'LinkedIn',
-        url: '',
+        url: 'https://www.linkedin.com/in/jorgechato/',
       },
     ],
   },
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,50 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class', '[data-mode="dark"]'],
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    screens: {
+      sm: '800px',
+    },
+    extend: {
+      colors: {
+        bg: 'var(--colors-bg)',
+        body: 'var(--colors-body)',
+        headings: 'var(--colors-headings)',
+        primary: 'var(--colors-primary)',
+        'primary-light': 'var(--colors-primary-light)',
+        'primary-lightest': 'var(--colors-primary-lightest)',
+        secondary: 'var(--colors-secondary)',
+        tertiary: 'var(--colors-tertiary)',
+        quaternary: 'var(--colors-quaternary)',
+        black: 'var(--colors-black)',
+        white: 'var(--colors-white)',
+        'active-link': 'var(--colors-active-link)',
+        grey: {
+          lightest: 'var(--colors-grey-lightest)',
+          light: 'var(--colors-grey-light)',
+          medium: 'var(--colors-grey-medium)',
+          dark: 'var(--colors-grey-dark)',
+          darker: 'var(--colors-grey-darker)',
+          darkest: 'var(--colors-grey-darkest)',
+        },
+      },
+      borderColor: (theme) => ({
+        default: theme('colors.grey-light'),
+      }),
+    },
+  },
+  variants: {
+    borderStyle: ['responsive', 'hover', 'focus'],
+    display: ['dark', 'responsive'],
+    opacity: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+    scale: ['responsive', 'hover', 'focus', 'group-hover'],
+    textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+    translate: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
   },
   plugins: [],
 }
