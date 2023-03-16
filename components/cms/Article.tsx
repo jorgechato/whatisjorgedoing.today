70330c313631170585c86b0552143c162352a263
jorgechato <hack@jorgechato.com>
2023-03-16 22:15:37 +0900

    UPDATE article/s component

--- components/cms/Article.tsx
+++ components/cms/Article.tsx
@@ -1,17 +1,32 @@
+import { format } from 'date-fns'
+import Markdown from 'markdown-to-jsx';
+
 import { GetArticleContent } from './FetchData';
+import { H1Font } from '../Fonts';
 
 
 export function Article(props: any) {
     const slug = props.slug;
     const article = GetArticleContent(slug);
+    const date = format(new Date(article.data.date), 'dd LLL yyyy');
 
     return (
         <>
-            <h1>{article.data.title}</h1>
-            <h2>{article.data.date}</h2>
-            <div>
-            {article.content}
-            </div>
+            <article className="container mx-auto px-4">
+                <header className="mb-12">
+                    <h1 className="text-3xl text-center">
+                        <span className={H1Font.className}>
+                            {article.data.title}
+                        </span>
+                    </h1>
+                    <div className="container font-display text-grey-darkest mx-auto px-4 text-xs mt-24 mb-12 tracking-wide text-center">
+                        {date}
+                    </div>
+                </header>
+                <Markdown className="text-justify text-base">
+                    {article.content}
+                </Markdown>
+            </article>
         </>
     );
 };
--- components/cms/ArticleList.tsx
+++ components/cms/ArticleList.tsx
@@ -1,27 +1,29 @@
 import Link from 'next/link';
 
+import { format } from 'date-fns'
+
 import { ArticleMetadata } from './ArticleMetadata';
 import { GetArticlesMetadata } from './FetchData';
 
 
-export function ArticleList() {
-    const articlesMetadata: ArticleMetadata[] = GetArticlesMetadata();
+export function ArticleList(props: any) {
+    const articlesMetadata: ArticleMetadata[] = GetArticlesMetadata(props.limit);
 
-    const articlesPreview = articlesMetadata.map((article) => (
+    const articlesPreview = articlesMetadata.map((article) => {
+        const date = format(new Date(article.date), 'dd LLL yyyy');
+
+        return (
         <li key={article.slug}>
             <Link href={article.slug}>
-                <span className="border-b border-grey-darker border-dotted hover:border-none hover:bg-black hover:text-white">
+                <span className="font-bold border-b border-grey-darker border-dotted hover:border-none hover:bg-black hover:text-white">
                     {article.title}
                 </span>
                 <time dateTime="2019-08-31T00:00:00Z" className="text-grey-darkest text-sm sm:ml-2 w-full sm:w-24 inline-block">
-                    Aug 31, 2019
+                    { date }
                 </time>
-            <div className="text-grey-darkest text-sm">
-                {article.summary}
-            </div>
             </Link>
         </li>
-    ));
+    )});
 
     return (
         <>
