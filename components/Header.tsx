db41b3617ba639650596038d4464b2e16c4e797a
jorgechato <hack@jorgechato.com>
2023-03-16 22:16:27 +0900

    UPDATE header with only the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,41 +1,19 @@
+'use client';
 import Link from 'next/link'
-import localFont from 'next/font/local';
 
-
-const titleFont = localFont({
-    src: [
-        {
-            path: './fonts/futura-medium.ttf',
-            style: 'normal',
-            weight: '500',
-        },
-        {
-            path: './fonts/futura-bold-new.ttf',
-            style: 'blod',
-            weight: '700',
-        },
-    ],
-});
+import { TitleFont } from './Fonts';
+import { ThemeButton } from './ThemeButton';
 
 
 export function Header() {
     const appName = process.env.TITLE;
 
-    const themeSwitcher = (
-        <button type="button" className="js-theme-toggle ml-auto text-current group relative leading-none" aria-label="Switch between Dark and Light mode">
-            <span className="absolute transform transition duration-100 right-0 inset-y-0 block mr-2 opacity-0 group-hover:-translate-x-4 group-hover:opacity-100 pointer-events-none">
-                <span className="hidden dark:block">light</span>
-                <span className="dark:hidden">dark</span></span>
-            <span className="block border-2 border-black hover:bg-black rounded-full w-3 h-3"></span>
-        </button>
-    );
-
     return (
         <header className="container mx-auto p-4 mt-4 mb-12 sm:mb-24 flex items-center text-sm">
-            <Link href="/" className={titleFont.className} style={{ fontWeight:'bold' }}>
+            <Link href="/" className={TitleFont.className} style={{ fontWeight:'bold' }}>
                 <div dangerouslySetInnerHTML={{ __html: appName }}></div>
             </Link>
-            {themeSwitcher}
+            <ThemeButton/>
         </header>
     )
 }
