8005544112c493437092a7b2c8fcf70c805c7cf2
jorgechato <hack@jorgechato.com>
2023-03-18 17:31:43 +0900

    ADD md code font && set theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: 'class',
@@ -36,6 +35,103 @@ module.exports = {
       borderColor: (theme) => ({
         default: theme('colors.grey-light'),
       }),
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            '*,*::before,*::after': {
+              borderColor: theme('colors.grey.light'),
+            },
+            color: theme('colors.body'),
+            h1: {
+              color: theme('colors.headings'),
+              fontSize: theme('fontSize.2xl'),
+              fontWeight: theme('fontWeight.medium'),
+            },
+            h2: {
+              color: theme('colors.headings'),
+              fontSize: theme('fontSize.xl'),
+              fontWeight: theme('fontWeight.medium'),
+            },
+            h3: {
+              color: theme('colors.headings'),
+              fontSize: theme('fontSize.lg'),
+              fontWeight: theme('fontWeight.medium'),
+            },
+            h4: {
+              color: theme('colors.headings'),
+              fontSize: theme('fontSize.base'),
+              fontWeight: theme('fontWeight.medium'),
+            },
+            h5: {
+              color: theme('colors.headings'),
+              fontSize: theme('fontSize.sm'),
+              fontWeight: theme('fontWeight.medium'),
+            },
+            h6: {
+              color: theme('colors.headings'),
+              fontSize: theme('fontSize.sm'),
+              fontWeight: theme('fontWeight.medium'),
+            },
+            'ul > li::before': {
+              content: '""',
+              position: 'absolute',
+              backgroundColor: theme('colors.grey.dark'),
+              borderRadius: '50%',
+            },
+            a: {
+              borderBottom: `1px dotted ${theme('colors.grey.darker')}`,
+              color: theme('colors.current'),
+              fontWeight: theme('fontWeight.medium'),
+              textDecoration: 'none',
+              '&:hover': {
+                backgroundColor: theme('colors.black'),
+                color: theme('colors.white'),
+                borderBottom: 'none',
+              },
+            },
+            pre: {
+              backgroundColor: theme('colors.bg'),
+              border: `1px solid ${theme('colors.grey.light')}`,
+              color: theme('colors.body'),
+              fontFamily: theme('fontFamily.mono'),
+            },
+            'pre code': {
+              fontSize: theme('text.sm'),
+            },
+            'pre code::before': {
+              content: 'inherit',
+            },
+            'pre code::after': {
+              content: 'inherit',
+            },
+            code: {
+              color: theme('colors.body'),
+              fontFamily: theme('fontFamily.mono'),
+            },
+            strong: {
+              color: theme('colors.body'),
+              fontWeight: theme('fontWeight.medium'),
+            },
+            thead: {
+              borderBottomColor: theme('colors.grey.light'),
+            },
+            tbody: {
+              tr: {
+                borderBottomColor: theme('colors.grey.light'),
+              },
+            },
+            figure: {
+              figcaption: {
+                color: theme('colors.grey.darkest'),
+                fontFamily: theme('fontFamily.display'),
+              },
+            },
+            blockquote: {
+              color: theme('colors.grey.darkest'),
+            },
+          },
+        },
+      }),
     },
   },
   variants: {
@@ -46,5 +142,7 @@ module.exports = {
     textColor: ['responsive', 'hover', 'focus', 'group-hover'],
     translate: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
