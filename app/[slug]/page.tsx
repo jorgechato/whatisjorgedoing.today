88489630e8022fb92932984431df90d45dae6891
jorgechato <hack@jorgechato.com>
2023-03-18 16:17:18 +0900

    UPDATE make articles real static pages

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,5 +1,12 @@
 import { Article } from '@/components/cms/Article';
+import { GetArticlesMetadata } from '@/components/cms/FetchData';
+import { ArticleMetadata } from '@/components/cms/ArticleMetadata';
 
+
+export const generateStaticParams = async () => {
+    const articlesMetadata: ArticleMetadata[] = GetArticlesMetadata();
+    return articlesMetadata.map((article: ArticleMetadata) => ({slug: article.slug}));
+};
 
 export default function ArticlePage(props: any) {
     const slug = props.params.slug;
