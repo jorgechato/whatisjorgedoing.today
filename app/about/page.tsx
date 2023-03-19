1d21c804fb61416a98b581ac16cadf6b055f1a3d
jorgechato <hack@jorgechato.com>
2023-03-20 00:57:52 +0900

    ADD metadata title

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: 'This is not a simulation or is it?',
+};
+
 export default function About() {
     return (
         <>
--- app/archive/page.tsx
+++ app/archive/page.tsx
@@ -1,6 +1,10 @@
 import { ArticleList } from '@/components/cms/ArticleList';
 
 
+export const metadata = {
+  title: 'The secret Archive',
+};
+
 export default function Home() {
     return (
         <>
