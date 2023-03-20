1b8e30e6d37846505307b051f3c6119bbe2140a8
jorgechato <hack@jorgechato.com>
2023-03-20 18:00:02 +0900

    UPDATE separate config from next

---
+++ components/ConfigType.ts
@@ -0,0 +1,19 @@
+export interface SiteMap {
+    name: string;
+    url: string;
+    location: string;
+}
+
+
+export interface Social {
+    name: string;
+    url: string;
+}
+
+
+export interface Echosystem {
+    name: string;
+    description: string;
+    url: string;
+    color: string;
+}
--- next.config.js
+++ next.config.js
@@ -3,61 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  env: {
-    ARTICLES_LOCATION: 'content/articles',
-    TITLE: 'WHAT IS JORGE</br>DOING TODAY?',
-    SITE_MAP: [
-      {
-        name: 'Home',
-        url: '/',
-        location: 'content/home.md',
-      },
-      {
-        name: 'About',
-        url: '/about',
-        location: 'content/about.md',
-      },
-      {
-        name: 'Archive',
-        url: '/archive',
-      },
-    ],
-    ECHOSYSTEM: [
-      {
-        name: 'JORGE</br>CHATO',
-        description: 'Personal website',
-        url: 'https://jorgechato.com',
-        color: '',
-      },
-      {
-        name: 'WHERE IS</br>JORGE TODAY?',
-        description: 'Funny project to track my location and events',
-        url: 'https://whereisjorge.today',
-        color: '',
-      },
-      {
-        name: 'NO APTO</br>EN ASIA',
-        description: 'Podcast about my experience in Asia',
-        url: 'https://noaptoen.asia',
-        color: '',
-      },
-    ],
-
-    SOCIALS: [
-      {
-        name: 'GitHub',
-        url: 'https://github.com/jorgechato',
-      },
-      {
-        name: 'Twitter',
-        url: 'https://twitter.com/jorgechato',
-      },
-      {
-        name: 'LinkedIn',
-        url: 'https://www.linkedin.com/in/jorgechato/',
-      },
-    ],
-  },
 }
 
 module.exports = nextConfig;
