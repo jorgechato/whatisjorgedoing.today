d30b0b1d958bfca6372208ec749cf169e1704c44
jorgechato <hack@jorgechato.com>
2023-03-28 19:42:28 +0900

    ADD env dep

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  env: {
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+    GITHUB_USERNAME: process.env.GITHUB_USERNAME,
+  },
 }
 
 module.exports = nextConfig;
