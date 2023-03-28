aa190992a42e65681b674b5804aa80a083c7571b
jorgechato <hack@jorgechato.com>
2023-03-28 19:44:30 +0900

    ADD projects section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 
 import { ArticleList } from '@/components/cms/ArticleList';
 import { H1Font } from '@/components/Fonts';
+import { ProjectList } from '@/components/git/ProjectList';
 
 
 export const metadata = {
@@ -11,12 +12,17 @@ export const metadata = {
 export default function Home() {
     return (
         <>
+            <div className={`text-2xl mb-4 ${H1Font.className}`} style={{ fontWeight: 'bold' }}>
+                <span className="text-grey-darkest">#</span> Projects
+            </div>
+            <ProjectList/>
+
             <Link href="/archive">
-                <span className={`text-2xl mb-4 ${H1Font.className}`} style={{ fontWeight:'bold' }}>
+                <span className={`text-2xl mb-4 ${H1Font.className}`} style={{ fontWeight: 'bold' }}>
                     <span className="text-grey-darkest">#</span> Archive
                 </span>
             </Link>
-            <ArticleList limit="5"/>
+            <ArticleList limit="5" />
             <Link href="/archive">
                 <span className="text-grey-darkest text-sm sm:ml-2 w-full sm:w-24">
                     ... More
