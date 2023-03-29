26a204f944077247b0614f38acecc17f76de883f
jorgechato <hack@jorgechato.com>
2023-03-29 14:06:33 +0900

    UPDATE analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,13 @@
 'use client';
 import './globals.css';
-import { usePathname, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
 
-import ReactGA from 'react-ga4';
+import { Analytics } from '@vercel/analytics/react';
 
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-    const GA_MEASUREMENT_ID: string = process.env.GA_MEASUREMENT_ID as string;
-    const pathname = usePathname();
-    const searchParams = useSearchParams();
-
-    useEffect(() => {
-        ReactGA.initialize(GA_MEASUREMENT_ID);
-        if (pathname) {
-            console.log(pathname + searchParams.toString());
-            ReactGA.send({
-                hitType: "pageview",
-                page: pathname + searchParams.toString(),
-            });
-        }
-    }, [pathname, searchParams]);
-
     return (
         <html lang="en" className="overflow-x-hidden dark">
             <body className="bg-bg antialiased font-body bg-bg leading-7 text-body">
@@ -35,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                     </div>
                 </main>
                 <Footer />
+                <Analytics />
             </body>
         </html>
     )
