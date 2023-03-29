9975b4e2002e84a27a7f6269f65b3e25777443eb
jorgechato <hack@jorgechato.com>
2023-03-29 13:04:00 +0900

    ADD ga integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,30 @@
+'use client';
 import './globals.css';
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+import ReactGA from 'react-ga4';
+
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-    //TODO: use document to add classes on body tag
+    const GA_MEASUREMENT_ID: string = process.env.GA_MEASUREMENT_ID as string;
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+
+    useEffect(() => {
+        ReactGA.initialize(GA_MEASUREMENT_ID);
+        if (pathname) {
+            console.log(pathname + searchParams.toString());
+            ReactGA.send({
+                hitType: "pageview",
+                page: pathname + searchParams.toString(),
+            });
+        }
+    }, [pathname, searchParams]);
+
     return (
         <html lang="en" className="overflow-x-hidden dark">
             <body className="bg-bg antialiased font-body bg-bg leading-7 text-body">
