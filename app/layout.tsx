32a8659da0c04b739a81833d5e44240108d84ff3
jorgechato <hack@jorgechato.com>
2023-03-30 09:58:57 +0900

    DELETE duplicate class

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { Footer } from '@/components/Footer';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en" className="overflow-x-hidden dark">
-            <body className="bg-bg antialiased font-body bg-bg leading-7 text-body">
+            <body className="bg-bg antialiased font-body leading-7 text-body">
                 <Header />
                 <main id="content">
                     <div className="container mx-auto px-4">
