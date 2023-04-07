4460e1e9e049f0d59399b761f099feb3b06c3d89
jorgechato <hack@jorgechato.com>
2023-04-07 18:09:32 +0900

    UPDATE logo as component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,9 @@
 'use client';
-import Link from 'next/link'
 import { useEffect } from 'react';
 
-import { TitleFont } from './Fonts';
 import { ThemeButton } from './ThemeButton';
 import { RegisterLanguages } from './git/RegisterLanguages';
+import { Logo } from './Logo';
 
 const config: { [key: string]: any } = require('@/my.config.js');
 
@@ -12,15 +11,13 @@ const config: { [key: string]: any } = require('@/my.config.js');
 export function Header() {
     const appName = config.TITLE || '';
 
-  useEffect(() => {
+    useEffect(() => {
         RegisterLanguages();
-  }, []);
+    }, []);
 
     return (
         <header className="container mx-auto p-4 mt-4 mb-12 sm:mb-24 flex items-center text-sm">
-            <Link href="/">
-                <div dangerouslySetInnerHTML={{ __html: appName }} className={TitleFont.className} style={{ fontWeight: 'bold' }}></div>
-            </Link>
+            <Logo url="/" appName={appName} />
             <ThemeButton />
         </header>
     )
