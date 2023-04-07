901c2e509bd0b779ca37f158034ce3f103055865
jorgechato <hack@jorgechato.com>
2023-04-07 18:11:35 +0900

    UPDATE indent and imports

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { ArticleList } from '@/components/cms/ArticleList';
 import { H1Font } from '@/components/Fonts';
 import { ProjectList } from '@/components/git/ProjectList';
+import { Status } from '@/components/git/Status';
 
 
 export const metadata = {
@@ -15,14 +16,14 @@ export default function Home() {
             <div className={`text-xl mb-4 ${H1Font.className}`} style={{ fontWeight: 'bold' }}>
                 <span className="text-grey-darkest">#</span> Projects
             </div>
-            <ProjectList/>
+            <ProjectList />
 
             <Link href="/archive">
                 <div className={`text-xl ${H1Font.className} my-4`} style={{ fontWeight: 'bold' }}>
                     <span className="text-grey-darkest">#</span> Archive
                 </div>
             </Link>
-            <ArticleList limit="5"/>
+            <ArticleList limit="5" />
             <Link href="/archive">
                 <span className="text-grey-darkest text-sm sm:ml-2 w-full sm:w-24">
                     ... More
