73c787edebec9f524433b3eeb431e6c041494c28
jorgechato <hack@jorgechato.com>
2023-04-07 18:09:15 +0900

    UPDATE logo with params

---
+++ components/Logo.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { useEffect, useState } from 'react';
+
+import { Status, StatusType } from '@/lib/status/FetchData';
+import Link from 'next/link'
+
+import { TitleFont } from './Fonts';
+import { Indicator } from './status/Indicator';
+
+
+export function Logo({ appName, url, discover }: { appName: string, url: string, discover?: boolean }) {
+    const [status, setStatus] = useState<Status | null>(null);
+    
+    return (
+        <Link href={url} className="relative">
+            <div dangerouslySetInnerHTML={{ __html: appName }} className={TitleFont.className} style={{ fontWeight: 'bold' }}></div>
+            {discover && <Indicator type={StatusType.OK}/>}
+        </Link>
+    )
+}
