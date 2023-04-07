082c657d10dc18ac643eee2bb0578f858028f3d6
jorgechato <hack@jorgechato.com>
2023-04-07 18:10:38 +0900

    ADD status indicator component

---
+++ components/git/Status.tsx
@@ -0,0 +1,39 @@
+'use client';
+import { useEffect, useState } from 'react'
+import { GetStatus } from './GetData'
+
+
+export function Status() {
+    const [status, setStatus] = useState<string>("");
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        GetStatus().then((data) => {
+            setStatus(data);
+            setLoading(false);
+        });
+    }, []);
+
+    const statusJSX = () => {
+        if (loading) {
+            return "Loading...";
+        }
+        return (
+            <div className="text-center">
+                <span className="cursor-pointer inline-flex items-center rounded-full p-2 bg-accent text-white group transition-all duration-500 focus:outline-none" role="alert" tabindex="0">
+                    <div className="w-6 h-6"></div>
+
+                    <span className="whitespace-nowrap inline-block group-hover:max-w-auto group-focus:max-w-screen-2xl max-w-0 scale-80 group-hover:scale-100 overflow-hidden transition-all duration-500 group-hover:px-2 group-focus:px-2">
+                        {status}
+                    </span>
+                </span>
+            </div>
+        );
+    };
+
+    return (
+        <>
+            {statusJSX()}
+        </>
+    )
+}
---
+++ components/status/Indicator.tsx
@@ -0,0 +1,29 @@
+import { StatusType } from "@/lib/status/FetchData";
+
+
+export function Indicator({ type }: { type: StatusType }) {
+    const indicatorClass = {
+        [StatusType.OK]: 'bg-green-500',
+        [StatusType.DEGRADED]: 'bg-yellow-500',
+        [StatusType.MAJOR]: 'bg-red-500',
+        [StatusType.MINOR]: 'bg-orange-500',
+        [StatusType.MAINTENANCE]: 'bg-violet-500',
+        [StatusType.UNKNOWN]: 'bg-gray-500',
+    }[type];
+
+    const indicatorLightClass = {
+        [StatusType.OK]: 'bg-green-400',
+        [StatusType.DEGRADED]: 'bg-yellow-400',
+        [StatusType.MAJOR]: 'bg-red-400',
+        [StatusType.MINOR]: 'bg-orange-400',
+        [StatusType.MAINTENANCE]: 'bg-violet-400',
+        [StatusType.UNKNOWN]: 'bg-gray-400',
+    }[type];
+
+    return (
+        <span className="top-0 left-0 relative flex h-3 w-3">
+            <span className={`animate-ping absolute inline-flex h-full w-full rounded-full ${indicatorLightClass} opacity-75`}></span>
+            <span className={`relative inline-flex rounded-full h-3 w-3 ${indicatorClass}`}></span>
+        </span>
+    );
+}
