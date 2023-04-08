1379aa870610979ad492e2d26d976eb1b43bd17b
jorgechato <hack@jorgechato.com>
2023-04-08 20:49:08 +0900

    ADD card for project status

--- components/status-page/Project.tsx
+++ components/status-page/Project.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect, useState } from 'react';
 
-import { Status, StatusType } from '@/lib/status-page/FetchData';
+import { GetStatus, Status, StatusType } from '@/lib/status-page/FetchData';
 import { Indicator } from './Indicator';
 import { Logo } from '../Logo';
 
@@ -11,21 +11,41 @@ export function Project({ url, name }: { url: string, name: string }) {
     const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
+        GetStatus(url, name).then((status: Status) => {
+            setStatus(status);
+            console.log(status);
+            setLoading(false);
+        });
     }, []);
+
+    const card = (status: Status) => {
+        let baseUrl: string;
+        try {
+            baseUrl = new URL(status.url).origin;
+        } catch (_) { baseUrl = "/" }
+
+        return (
+            <Logo url={baseUrl} appName={status.name}>
+                <div className='relative text-xs text-grey-dark border border-grey-light rounded-md p-4 m-4 select-none'>
+                    <p className='text-grey-darker absolute -top-2 left-1/2 transform -translate-x-1/2 bg-bg px-2'>
+                        {status.version}
+                    </p>
+                    <div className='absolute -top-1 -left-1'>
+                        <Indicator type={status.type} />
+                    </div>
+                    Watching:
+                    <p className="font-bold text-grey-darker">{status.watching}</p>
+                </div>
+            </Logo>
+        );
+    };
 
     return (
         <>
-            {loading &&
-                <Logo url={url} appName={name}>
-                    <Indicator type={StatusType.UNKNOWN} />
-                </Logo>
-            }
-
-            {!loading && status &&
-                <Logo url={status.url} appName={status.name}>
-                    <Indicator type={status.type} />
-                </Logo>
-            }
+            <div className="m-4 transition duration-500 hover:scale-110">
+                {loading && card({ url: url, watching: url, name: name, type: StatusType.UNKNOWN })}
+                {!loading && status && card(status)}
+            </div>
         </>
     );
 }
