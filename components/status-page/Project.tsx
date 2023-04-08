cfc8d340724cf1e905992662c2d6ccc42d93ba91
jorgechato <hack@jorgechato.com>
2023-04-08 12:26:49 +0900

    UPDATE bones for status page projects

--- components/status-page/Project.tsx
+++ components/status-page/Project.tsx
@@ -1,5 +1,31 @@
-export function Project() {
+'use client';
+import { useEffect, useState } from 'react';
+
+import { Status, StatusType } from '@/lib/status-page/FetchData';
+import { Indicator } from './Indicator';
+import { Logo } from '../Logo';
+
+
+export function Project({ url, name }: { url: string, name: string }) {
+    const [status, setStatus] = useState<Status>();
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+    }, []);
+
     return (
-        <></>
+        <>
+            {loading &&
+                <Logo url={url} appName={name}>
+                    <Indicator type={StatusType.UNKNOWN} />
+                </Logo>
+            }
+
+            {!loading && status &&
+                <Logo url={status.url} appName={status.name}>
+                    <Indicator type={status.type} />
+                </Logo>
+            }
+        </>
     );
 }
--- components/status-page/ProjectList.tsx
+++ components/status-page/ProjectList.tsx
@@ -1,11 +1,20 @@
+import { Status } from "@/lib/ConfigType";
+import { Project } from "./Project";
+
+
+const config: { [key: string]: any } = require('@/my.config.js');
+
 export function ProjectList() {
+    const projects: Status[] = config.STATUS;
+
     return (
         <>
             <ul className='grid text-3xl md:grid-cols-2 xs:grid-cols-1 gap-4 mt-10'>
-                <li className='text-center'>
-                </li>
-                <li className='text-center'>
-                </li>
+                {projects.map((project: Status, index: number) => (
+                    <li key={index} className='text-center'>
+                    <Project url={project.url} name={project.name} />
+                    </li>
+                ))}
             </ul>
         </>
     );
