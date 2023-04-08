984d985f88d5e5794d1bbdf81d7aa8ce98f1efc3
jorgechato <hack@jorgechato.com>
2023-04-08 11:37:00 +0900

    ADD Project & ProjectList for status page

---
+++ components/status-page/ProjectList.tsx
@@ -0,0 +1,12 @@
+export function ProjectList() {
+    return (
+        <>
+            <ul className='grid text-3xl md:grid-cols-2 xs:grid-cols-1 gap-4 mt-10'>
+                <li className='text-center'>
+                </li>
+                <li className='text-center'>
+                </li>
+            </ul>
+        </>
+    );
+}
