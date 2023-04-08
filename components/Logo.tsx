9e0d4a08757debe143f143d15874a94b2bdcf733
jorgechato <hack@jorgechato.com>
2023-04-08 12:26:20 +0900

    UPDATE move fetch status from logo to children

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,20 +1,14 @@
-'use client';
-import { useEffect, useState } from 'react';
-
-import { Status, StatusType } from '@/lib/status-page/FetchData';
 import Link from 'next/link'
 
 import { TitleFont } from './Fonts';
-import { Indicator } from './status-page/Indicator';
 
 
-export function Logo({ appName, url, discover }: { appName: string, url: string, discover?: boolean }) {
-    const [status, setStatus] = useState<Status | null>(null);
+export function Logo({ appName, url, children }: { appName: string, url: string, children?: any}) {
     
     return (
         <Link href={url} className="relative">
             <div dangerouslySetInnerHTML={{ __html: appName }} className={TitleFont.className} style={{ fontWeight: 'bold' }}></div>
-            {discover && <Indicator type={StatusType.OK}/>}
+            {children}
         </Link>
     )
 }
