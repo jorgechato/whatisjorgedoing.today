e56a0e54e0563ee10d58b51554cdb2b9b7e1c073
jorgechato <hack@jorgechato.com>
2023-05-11 15:18:47 +0900

    FIX my config for sitemap & DELETE status+about

--- my.config.js
+++ my.config.js
@@ -10,34 +10,15 @@ module.exports = {
     },
     {
       name: 'Status',
-      url: '/status',
+      url: 'https://jorgechato.com/status',
     },
     {
       name: 'About',
-      url: '/about',
+      url: 'https://jorgechato.com/about',
     },
     {
       name: 'Archive',
       url: '/archive',
-    },
-  ],
-
-  STATUS: [
-    {
-      name: 'JORGE</br>CHATO',
-      url: 'https://jorgechato.com/api/health-check',
-    },
-    {
-      name: 'WHERE IS</br>JORGE TODAY?',
-      url: 'https://whereisjorge.today/api/health-check',
-    },
-    {
-      name: 'NO APTO</br>EN ASIA',
-      url: 'https://noaptoen.asia/health-check.json',
-    },
-    {
-      name: title,
-      url: '/api/health-check',
     },
   ],
 
